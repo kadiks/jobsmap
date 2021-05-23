06dea4a95b3abd5475468767744557cc246f47ff
Adeline <adeline.sire@gmail.com>
2021-05-23 15:30:26 +0200

    Creates route /jobs

---
+++ back/controllers/jobsController.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+const getJobs = (req, res) => {
+  const jobs = [
+    {
+      "city": "Paris",
+      "total": 1234
+    }
+    {
+      "city": "Marseille",
+      "total": 800
+    }
+  ]
+  res.json({
+    success: true,
+    data: jobs
+  });
+}
+
+router.route('/').get(getJobs);
--- back/index.js
+++ back/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
+//app
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// controllers
+const {
+  jobsController
+} = require('/controllers');
+
+// Routes
+app.use('./jobs', jobsController);
 
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
