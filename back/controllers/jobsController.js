ee47e82d979cb369312813fe6946613f41951fc7
Gregory Houldsworth <gregory.houldsworth@protonmail.com>
2021-05-23 20:04:05 +0200

    fix null coords on non-cities

--- back/controllers/jobsController.js
+++ back/controllers/jobsController.js
@@ -5,30 +5,11 @@ const getJobsFromAPI = require('../lib/get-jobs')
 
 const getJobs = async (req, res) => {
 
-  const jabs = await getJobsFromAPI(req.accessToken)
-  res.json({
-    success: true,
-    data: jabs
-  });
-  /*
-  console.log(jabs)
-  const jobs = [
-    {
-      "city": "Paris",
-      "total": 1234
-    },
-    {
-      "city": "Marseille",
-      "total": 800
-    }
-  ]
+  const jobs = await getJobsFromAPI(req.accessToken)
   res.json({
     success: true,
     data: jobs
   });
-  */
 }
-
-//router.route('/').get(getJobs);
 
 module.exports = { getJobs }
--- back/lib/get-jobs/index.js
+++ back/lib/get-jobs/index.js
@@ -1,3 +1,4 @@
+const { isNull } = require('lodash')
 const fetch = require('node-fetch')
 
 const jobs = {
@@ -9,18 +10,18 @@ const jobs = {
 module.exports = async function getJobs(token){
     const jobsAreFresh = (new Date() - jobs.lastSet) < 8.64e+7
     if(jobs.data && jobsAreFresh){
-        return jobs.data
+        return jobs.counts
     }
     else{
         jobs.data = []
         await requestJobs_(token)
         countJobs()
+        cleanJobs()
         return jobs.counts
     }
 }
 
 async function requestJobs_(token, index=0){
-    console.log(`round ${index}-${index+149}`)
     const url = `https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search?codeROME=M1805&range=${index}-${index+149}`
     const res = await fetch(url, { method: 'GET', headers: {Authorization: `Bearer ${token}`} })
     const json = await res.json()
@@ -43,6 +44,14 @@ function countJobs(){
                 total: 1,
                 coords: [entry.lieuTravail.latitude, entry.lieuTravail.longitude]
             }
+        }
+    }
+}
+
+function cleanJobs(){
+    for(const key in jobs.counts){
+        if(!jobs.counts[key].coords[0]){
+            delete jobs.counts[key]
         }
     }
 }
