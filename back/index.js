b6de45fd434d2c35e923bb8c7926a71627930230
Jénaïc Cambré <me@jenaiccambre.com>
2021-05-23 21:32:40 +0200

    serve public front

--- back/index.js
+++ back/index.js
@@ -1,7 +1,7 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const getToken = require('./lib/get-token')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const getToken = require("./lib/get-token");
 const PORT = process.env.PORT || 3002;
 
 // app
@@ -10,35 +10,38 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Serve static
+app.use(express.static("public"));
+
 // controllers
-const { jobsController } = require('./controllers');
+const { jobsController } = require("./controllers");
 
-let accessToken = null
+let accessToken = null;
 
-app.all('*', function(req, res, next){
-  if(!accessToken){
+app.all("*", function (req, res, next) {
+  if (!accessToken) {
     res.json({
       success: false,
-      data: 'server error: no b2b token'
-    })
-    return
-  }else{
-    req.accessToken = accessToken
-    next()    
+      data: "server error: no b2b token",
+    });
+    return;
+  } else {
+    req.accessToken = accessToken;
+    next();
   }
-})
+});
 
-async function init(){  
-  accessToken = await getToken()
+async function init() {
+  accessToken = await getToken();
   // Routes
-  app.use('/jobs/places', jobsController.getJobs);
+  app.use("/jobs/places", jobsController.getJobs);
   // set a new token
-  setInterval(async function(){
-    accesToken = await getToken()
+  setInterval(async function () {
+    accesToken = await getToken();
   }, 1499000);
 }
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
-  init()
+  init();
 });
