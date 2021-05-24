e4e48dbe1062d1b91aba37c59ea54eaaf05608d9
Jénaïc Cambré <me@jenaiccambre.com>
2021-05-24 11:59:30 +0200

    change to double quotes

--- back/controllers/jobsController.js
+++ back/controllers/jobsController.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const getJobsFromAPI = require('../lib/get-jobs')
+const getJobsFromAPI = require("../lib/get-jobs");
 
 const getJobs = async (req, res) => {
-  const jobs = await getJobsFromAPI(req.accessToken)
+  const jobs = await getJobsFromAPI(req.accessToken);
   res.json({
     success: true,
-    data: jobs
+    data: jobs,
   });
-}
+};
 
-module.exports = { getJobs }
+module.exports = { getJobs };
