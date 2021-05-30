ee4175e2c4cd2d9a12b9451ff3e292c1949b1a14
Julie Desvaux <desvaux.julie@gmail.com>
2021-05-30 15:15:05 +0200

    add query filter route jobs/places

--- back/controllers/jobsController.js
+++ back/controllers/jobsController.js
@@ -4,11 +4,37 @@ const router = express.Router();
 const getJobsFromAPI = require("../lib/get-jobs");
 
 const getJobs = async (req, res) => {
-  const jobs = await getJobsFromAPI(req.accessToken);
-  res.json({
-    success: true,
-    data: jobs,
-  });
+	let jobs = await getJobsFromAPI(req.accessToken);
+	const { order } = req.query;
+	switch (order) {
+		case "alpha":
+			jobs.sort(function compare(a, b) {
+				if (a.name < b.name) return -1;
+				if (a.name > b.name) return 1;
+				return 0;
+			});
+			res.json({
+				success: true,
+				data: jobs,
+			});
+			break;
+		case "offers":
+			jobs.sort(function compare(a, b) {
+				if (a.total > b.total) return -1;
+				if (a.total < b.total) return 1;
+				return 0;
+			});
+			res.json({
+				success: true,
+				data: jobs,
+			});
+			break;
+		default:
+			res.json({
+				success: true,
+				data: jobs,
+			});
+	}
 };
 
 module.exports = { getJobs };
