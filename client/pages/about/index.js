ab07c1cf0a5175961c08a762556c8855ee73a4a0
unknown <yousrah02@hotmail.com>
2021-05-30 16:09:55 +0200

    page statique

--- client/pages/about/index.js
+++ client/pages/about/index.js
@@ -12,56 +12,70 @@ export default function About() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.mission}>
-        <h2 className={styles.title}>
-          Mission
-        </h2>
 
-        <p className={styles.description}>
-        Rechercher un métier dans la programmation lorsqu'on se reconverti professionnellement est souvent impressionant :
-        </p>
+        <div className={styles.intro}>
+          <h2 className={styles.title}>
+            Mission
+          </h2>
+          <div className={styles.cardAbout}>
 
-        <ul className={styles.description}>
-            <li>
-            - on ne sait pas quelle branche du métier choisir
-            </li>
-            <li>
-            - on se demande si la branche choisie permet de trouver un boulot dans sa région
-            </li>
-            <li>
-            - les formations nous persuadent que leur choix de technologies (langages) est le meilleur, mais comment en être sûr·e ?
-            </li>
-        </ul>
-        </div>
+          <p className={styles.description}>
+          Rechercher un métier dans la programmation lorsqu'on se reconverti professionnellement est souvent impressionant :
+          </p>
 
-        <h2 className={styles.title}>
-            Philosophie
-        </h2>
+          <ul className={styles.description}>
+
+              <li>
+              - on ne sait pas quelle branche du métier choisir
+              </li>
+              <li>
+              - on se demande si la branche choisie permet de trouver un boulot dans sa région
+              </li>
+              <li>
+              - les formations nous persuadent que leur choix de technologies (langages) est le meilleur, mais comment en être sûr·e ?
+              </li>
+
+          </ul>
+          </div>
+
+          <h2 className={styles.title}>
+              Philosophie
+          </h2>
+
+        <div className={styles.cardAbout}>
+          
+
         <p className={styles.description}>Nous voulions créer un projet à impact social et apprendre de nouvelles compétences en programmation par la même occasion.
         Ce projet est open source et si vous voulez y contribuer, n'hésitez à contacter l'un de nous. 
         </p>
 
+        </div>
+        </div>
+
         <div className={styles.grid}>
+
           <a href="https://www.linkedin.com/in/yousra-h-919ba0198/" className={styles.card}>
+            <Image src="/Yousra.jpg"  width={100} height={100} className={styles.image}/>
             <h2>Yousra H (Développeuse Next.js / React) &rarr;</h2>
-            <Image src="/photo.jpg"  width={100} height={100} className={styles.image}/>
+          </a>
+
+          <a href="https://www.linkedin.com/in/julie-desvaux/" className={styles.card}>
+            <Image src="/Julie.jpg"  width={100} height={100} className={styles.image}/>
+            <h2>Julie Desvaux (Développeuse Next.js / React) &rarr;</h2>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
+            <Image src="/Adeline.jpg"  width={100} height={100} className={styles.image}/>
             <h2>Adeline S (Développeuse Node.JS / Express) &rarr;</h2>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
+            <Image src="/Gregory.jpg"  width={100} height={100} className={styles.image}/>
             <h2>Gregory H (Développeur Node.JS / Express) &rarr;</h2>
           </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
+          <a href="https://www.linkedin.com/in/jenaiccambre/" className={styles.card}>
+            <Image src="/Jenaic.jpg"  width={100} height={100} className={styles.image}/>
             <h2>Jénaïc C (Développeur Next.js / React) &rarr;</h2>
           </a>
         </div>
